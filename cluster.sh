9c4db661a3d0a61ec6057050cbd62ef92bc617de
p-hofmann <peter.hofmann@hhu.de>
2014-11-05 12:11:11 +0000

    use pre-calculated ref distances to improve time

--- cluster.sh
+++ cluster.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#HOME_DIR="/home/user03/pyprojects/taxonomic_classification_plus_otu/"
+#t1="/home/user03/output/nobackup/2014_09_24_random1000_ref_silva_hmmer3/16S_rRNA.fna"
+#t2="/home/user03/output/nobackup/2014_09_24_random1000_ref_silva_hmmer3/mothur_cluster_list.txt"
+#t3="/home/user03/pyprojects/taxonomic_classification_plus_otu/references/"
+#t4="0.05"
+#t5="no longer used"
+#t6="45"
 
-#if [ "$#" -eq 4 ]; then
-#	FAST_TREE=0
-#elif [ "$#" -eq 5 ]; then
-#	FAST_TREE="$5"
-#elif [ "$#" -ne 4 ]; then
-#	echo "Illegal number of parameters: $#"
-#	echo "$0 marker_gene.fasta output_otu.txt reference_db.align cluster_cutoff fast_tree"
-#	exit;
-#fi
+HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #getting passed arguments
 INPUT_FASTA_FILE=$(basename "$1")
 OUTPUT_FILE="$2"
-OUTPUT_FOLDER=$(dirname "$2")
+OUTPUT_FOLDER=$(dirname "$OUTPUT_FILE")
 extension="${INPUT_FASTA_FILE##*.}"
 filename="${INPUT_FASTA_FILE%.*}"
 
 INPUT_FASTA="$1"
-REFERENCE_DB_ALIGNMENTS="$3"
+SILVA_REFERENCE_DIRECTORY="$3"
 CLUSTER_CUTOFF="$4"
 FAST_TREE="$5"
 PROCESSORS="$6"
@@ -39,8 +37,8 @@ FASTA_TO_PHYLIP="$TOOL_DIR/Fasta2Phylip.pl"
 R_DIST_SCRIPT="$TOOL_DIR/dist_from_tree.R"
 
 #list of possible output file names from mothur
-MERGED_ALIGNMENT="$filename.merged.align"
-BAD_SEQ_FILE="$filename.flip.accnos"
+#MERGED_ALIGNMENT="$filename.merged.align"
+BAD_SEQ_FILE="$filename.unique.flip.accnos"
 
 
 ######################
@@ -54,45 +52,30 @@ SYMBOLIC_LINK="$TMP_DIR/$INPUT_FASTA_FILE"
 ln -s "$INPUT_FASTA" "$SYMBOLIC_LINK"
 cd "$TMP_DIR"
 
+LOCAL_ALIGN_DIST="ref.align.dist"
+REF_ALIGN_DIST="$SILVA_REFERENCE_DIRECTORY/mothur_ref_distances"
+REF_UNIQUE_NAMES="$SILVA_REFERENCE_DIRECTORY/mothur_ref_names"
+REF_FASTA="$SILVA_REFERENCE_DIRECTORY/mothur_alignment_ref.fasta"
+# ref data can be huge 53 GB, but needs copied since it will be merged with the new distances
+cp "$REF_ALIGN_DIST" "$LOCAL_ALIGN_DIST"
+
+INPUT_NAMES_FILE="$filename.names"
+INPUT_PICK_NAMES_FILE="$filename.pick.names"
+MERGED_NAMES="$filename.merged.names"
+
 echo "align.seqs"
-mothur_commands_00="align.seqs(candidate=$INPUT_FASTA_FILE, template=$REFERENCE_DB_ALIGNMENTS, align=gotoh, flip=t, processors=$PROCESSORS)
-remove.seqs(accnos=$BAD_SEQ_FILE, fasta=current)"
-echo -e "$mothur_commands_00" | "$MOTHUR"
-#exit;
-
-if [[ -f "$BAD_SEQ_FILE" ]]; then
-	mv "$BAD_SEQ_FILE" "$OUTPUT_FOLDER/" 2>/dev/null
-fi
-
-INPUT_ALIGN="$filename.align"
-INPUT_PICK_ALIGN="$filename.pick.align"
-INPUT_INPUT_ALIGN="$filename.input.align"
-
-if [[ -f "$INPUT_PICK_ALIGN" ]]; then
-	mv "$INPUT_PICK_ALIGN" "$INPUT_INPUT_ALIGN"
-elif [[ -f "$INPUT_ALIGN" ]]; then
-	mv "$INPUT_ALIGN" "$INPUT_INPUT_ALIGN"
-fi
-
-echo "clustering"
-mothur_commands_01="merge.files(input=$INPUT_INPUT_ALIGN-$REFERENCE_DB_ALIGNMENTS, output=$MERGED_ALIGNMENT)
-set.current(fasta=$MERGED_ALIGNMENT)
-unique.seqs()
-filter.seqs()
-dist.seqs(cutoff=$CLUSTER_CUTOFF, processors=$PROCESSORS, calc=onegap, countends=F)
-cluster(cutoff=$CLUSTER_CUTOFF, method=furthest, precision=1000)"
-#cluster.split(cutoff=$CLUSTER_CUTOFF, method=furthest, precision=1000, processors=$PROCESSORS)"
-#cluster(cutoff=$CLUSTER_CUTOFF, method=nearest, precision=10000)"
-echo -e "$mothur_commands_01" | "$MOTHUR"
-
-#RESULT_FILE="*.list"
-#if [[ ! -f "$RESULT_FILE" ]]; then
-#	echo -e "Clustering failed, no result!"
-#fi
+mothur_commands="unique.seqs(fasta=$INPUT_FASTA_FILE)
+align.seqs(candidate=current, template=$REF_FASTA, align=gotoh, flip=t, processors=$PROCESSORS)
+remove.seqs(accnos=$BAD_SEQ_FILE, fasta=current, name=current)
+dist.seqs(oldfasta=$REF_FASTA, column=$LOCAL_ALIGN_DIST, cutoff=$CLUSTER_CUTOFF, processors=$PROCESSORS, calc=onegap, countends=F)
+merge.files(input=$INPUT_NAMES_FILE-$REF_UNIQUE_NAMES, output=$MERGED_NAMES)
+merge.files(input=$INPUT_PICK_NAMES_FILE-$REF_UNIQUE_NAMES, output=$MERGED_NAMES)
+set.current(name=$MERGED_NAMES)
+cluster(cutoff=$CLUSTER_CUTOFF, method=furthest, precision=100)"
+echo -e "$mothur_commands" | "$MOTHUR"
 
 if mv *.list "$OUTPUT_FOLDER/mothur_otu.txt"; then
 # 2>/dev/null
-#	#clean up folders, removed for testing
 	echo "clean up folders"
 	rm -rf "$TMP_DIR"
 else
