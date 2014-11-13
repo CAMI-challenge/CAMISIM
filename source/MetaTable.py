23a0099035d61b54cf3f4fea92709a500df27ee1
p-hofmann <peter.hofmann@hhu.de>
2014-11-13 17:41:28 +0000

    reading table is now separate, added logging

--- source/MetaTable.py
+++ source/MetaTable.py
@@ -10,85 +10,96 @@ import re
 
 class MetaTable:
 	"""Reading and writing a meta table"""
-	def __init__(self, file_path=None, separator="\t", head=True):
-		self.number_of_rows = 0
-		self.header = []
-		self.clear()
-		self.file_path = file_path
-		self.separator = separator
-		if file_path is not None and os.path.isfile(file_path):
-			self.meta_table = self.read_meta_table_by_column(head)
-		elif file_path is not None:
-			print "Error (MetaTable): no file found at:", file_path
+	def __init__(self, separator="\t", logger=None):
+		self._logger = logger
+		self._number_of_rows = 0
+		self._header = []
+		self._meta_table = {}
+		self._separator = separator
 
 	def clear(self):
-		self.file_path = None
-		self.header = []
-		self.number_of_rows = 0
-		self.meta_table = {}
+		self._header = []
+		self._number_of_rows = 0
+		self._meta_table = {}
 
 	def remove_empty_columns(self):
-		new_header = list(self.header)
-		for title in self.header:
+		new_header = list(self._header)
+		for title in self._header:
 			column = set(self.get_column(title))
 			column = [value.strip() for value in column]
 			if len(column) == 1 and '' in column:
 				new_header.remove(title)
-		self.header = new_header
+		self._header = new_header
 
-
-	def read_meta_table_by_column(self, head=True):
-		meta_table = {}
-		with open(self.file_path) as file_handler:
+	def read(self, file_path, head=True):
+		self._meta_table = {}
+		if not os.path.isfile(file_path):
+			if self._logger:
+				self._logger.error("MetaTable: no file found at: '{}'".format(file_path))
+			return
+		with open(file_path) as file_handler:
 			if head:
-				self.header = file_handler.readline().strip().split(self.separator)
-				#print "headers", len(self.header), self.header
-				for column_name in self.header:
-					meta_table[column_name] = []
+				self._header = file_handler.readline().strip().split(self._separator)
+				for column_name in self._header:
+					self._meta_table[column_name] = []
 			for line in file_handler:
-				self.number_of_rows += 1
-				if line[0] != '#':
-					row = line.split(self.separator)
-					for index in range(0, len(row)):
-						if head:
-							meta_table[self.header[index]].append(row[index].strip())
-						else:
-							if index not in meta_table:
-								meta_table[index] = []
-							meta_table[index].append(row[index].strip())
-		return meta_table
+				if line.startswith('#'):
+					continue
+				self._number_of_rows += 1
+				row = line.split(self._separator)
+				for index in range(0, len(row)):
+					if head:
+						self._meta_table[self._header[index]].append(row[index].strip())
+					else:
+						if index not in self._meta_table:
+							self._meta_table[index] = []
+						self._meta_table[index].append(row[index].strip())
 
-	def write_meta_table_by_column(self, file_path):
+	def write(self, file_path, head=True):
 		with open(file_path, "w") as file_handler:
-			#number_of_rows = len(self.meta_table[self.header[0]])
-			header = ""
-			for value in self.header:
-				header += value + self.separator
-			file_handler.write(header.strip() + "\n")
-			for row_number in range(0, self.number_of_rows):
+			if head:
+				header = ""
+				for value in self._header:
+					header += value + self._separator
+				file_handler.write(header.rstrip('\t') + '\n')
+			for row_number in range(0, self._number_of_rows):
 				row = []
-				for value in self.header:
-					if len(self.meta_table[value]) > row_number:
-						row.append(str(self.meta_table[value][row_number]))
+				for value in self._header:
+					if len(self._meta_table[value]) > row_number:
+						row.append(str(self._meta_table[value][row_number]))
 					else:
 						row.append('')
-				file_handler.write(self.separator.join(row) + "\n")
+				file_handler.write(self._separator.join(row) + '\n')
+
+	def get_number_of_rows(self):
+		return self._number_of_rows
 
 	def get_entry_index(self, column_name, entry_name):
-		if entry_name in self.meta_table[column_name]:
-			return self.meta_table[column_name].index(entry_name)
+		if entry_name in self._meta_table[column_name]:
+			return self._meta_table[column_name].index(entry_name)
 		else:
 			return -1
 
 	def get_column(self, column_name):
-		if column_name in self.meta_table:
-			return self.meta_table[column_name]
+		if column_name in self._meta_table:
+			return self._meta_table[column_name]
 		else:
 			return None
 
-	def set_column(self, column_name, values=[]):
-		if column_name not in self.header:
-			self.header.append(column_name)
-		if self.number_of_rows == 0:
-			self.number_of_rows = len(values)
-		self.meta_table[column_name] = values
+	def get_empty_column(self, default_value=''):
+		return [default_value] * self._number_of_rows
+
+	def set_column(self, values=None, column_name=None):
+		if column_name is None:
+			column_name = len(self._header)
+		if values is None:
+			values = []
+		if self._number_of_rows == 0:
+			self._number_of_rows = len(values)
+		elif self._number_of_rows < len(values):
+			self._number_of_rows = len(values)
+			if self._logger:
+				self._logger.warning("MetaTable: Inconsistent length of columns!")
+		if column_name not in self._header:
+			self._header.append(column_name)
+		self._meta_table[column_name] = values
