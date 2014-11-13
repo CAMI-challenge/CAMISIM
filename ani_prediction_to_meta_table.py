066e9073db557d0a6ffb77e4bd3837c1dc754b89
p-hofmann <peter.hofmann@hhu.de>
2014-11-13 17:46:41 +0000

    cleanup, now using passed arguments only

--- ani_prediction_to_meta_table.py
+++ ani_prediction_to_meta_table.py
@@ -1,207 +1,54 @@
-#!/usr/bin/env python
-
 __author__ = 'hofmann'
 
-from source.MothurOTU import MothurOTU
+#from source.ArgumentHandler import ArgumentHandler
+from source.MothurCluster import MothurCluster
+from source.NcbiTaxonomy.NcbiTaxonomy import NcbiTaxonomy
 from source.MetaTable import MetaTable
-from source.TaxonomyNcbi import TaxonomyNcbi
-from source.config import Config
 from source.ANIm import ANIm
+from source.Logger import Logger
 import sys
 import os
-import argparse
-import logging
-import logging.handlers
-
-#original prototype:
-#   http://armchairbiology.blogspot.de/2013/11/ani-are-you-okay-are-you-okay-ani.html
-#   (c) The James Hutton Institute 2013
-#   Author: Leighton Pritchard
-#
-#   Contact:
-#   leighton.pritchard@hutton.ac.uk
-#   GNU General Public License
 
 
-def get_logger(verbose=False, logfile=None):
-	# We set up logging, and modify loglevel according to whether we need
-	# verbosity or not
-	# err_handler points to sys.stderr
-	# err_handler_file points to a logfile, if named
-	logger = logging.getLogger('calculate_ani.py')
-	logger.setLevel(logging.DEBUG)
-	err_handler = logging.StreamHandler(sys.stderr)
-	err_formatter = logging.Formatter('%(levelname)s: %(message)s')
-	err_handler.setFormatter(err_formatter)
-	if logfile is not None:
-		try:
-			log_stream = open(logfile, 'w')
-			err_handler_file = logging.StreamHandler(log_stream)
-			err_handler_file.setFormatter(err_formatter)
-			err_handler_file.setLevel(logging.INFO)
-			logger.addHandler(err_handler_file)
-		except:
-			logger.error("Could not open %s for logging" % logfile)
-			sys.exit(1)
-	if verbose:
-		err_handler.setLevel(logging.INFO)
-	else:
-		err_handler.setLevel(logging.WARNING)
-	logger.addHandler(err_handler)
-	return logger
+def my_main(options, logger=None):
+	if logger == None:
+		logger = Logger("ANI prediction")
+	#ranks = args.ranks.strip().split(',')
 
+	column_name_ani_novelty = options.column_name_ani_novelty
+	column_name_ani_taxid = options.column_name_ani_compare
+	column_name_ani_scientific_name = options.column_name_ani_scientific_name
+	column_name_ani = options.column_name_ani
 
-def my_main():
-	"""Parsing of arguments"""
-	# example:
-	epilog = '''
-'''
+	logger.info("Reading metadata table file: '{}".format(options.metadata_table_in))
+	metadata_table = MetaTable(options.metadata_table_in)
 
-	description = "Using average nucleotide identity to make novelty predictionw that are added to a metatable"
-	parser = argparse.ArgumentParser(description=description)
-	#parser = argparse.ArgumentParser(description=description, epilog=epilog)
+	logger.info("Loading taxonomic database: '{}'".format(options.ncbi_reference_directory))
+	taxonomy = NcbiTaxonomy(options.ncbi_reference_directory, False, logger)
 
-	parser.add_argument("-i", "--metadata_table_in", default=None, type=str,
-						help="")
-	parser.add_argument("-o", "--metadata_table_out", default=None, type=str,
-						help="")
-	parser.add_argument("-iq", "--input_querry_file", default=None, type=str,
-						help="path to file containing tab separated list of genomes and their file path")
-	parser.add_argument("-ir", "--input_ref_file", default=None, type=str,
-						help="path to file containing tab separated list of genomes and their file path")
-	parser.add_argument("-cl", "--mothur_otu_file", default=None, type=str,
-						help="")
-	parser.add_argument("-p", "--pool_size", default=10, type=int,
-						help="number of processors to be used. Default: 2")
-	parser.add_argument("-r", "--ranks", default="family,genus,species", type=str,
-						help="legal ranks, default: family,genus,species")
-	args = parser.parse_args()
+	cluster_file = os.path.join(options.project_directory, options.file_cluster_mg_16s)
+	logger.info("Reading mothur cluster file: '{}'".format(cluster_file))
+	mothur_cluster = MothurCluster()
+	with open(cluster_file, 'r') as file_handle:
+		mothur_cluster.read_mothur_clustering_file(file_handle)
 
-	metadata_table_in = args.metadata_table_in
-	if metadata_table_in is None:
-		print "Error -i: "
-		#parser.print_help()
-		sys.exit(1)
+	ani_scientific_name_column = metadata_table.get_empty_column()
+	ani_prediction_novelty_column = metadata_table.get_empty_column()
+	ani_prediction_column = metadata_table.get_empty_column()
+	ani_column = metadata_table.get_empty_column()
+	cutoff_column = metadata_table.get_column(options.column_name_cutoff)
+	unpublished_genome_ids_column = metadata_table.get_column(options.column_name_unpublished_genomes_id)
 
-	metadata_table_out = args.metadata_table_out
-	if metadata_table_out is None:
-		print "Error -o: "
-		#parser.print_help()
-		sys.exit(1)
-
-	input_querry_file = args.input_querry_file
-	if input_querry_file is None:
-		print "Error -iq: "
-		sys.exit(1)
-
-	input_ref_file = args.input_ref_file
-	if input_ref_file is None:
-		print "Error -ir: "
-		sys.exit(1)
-
-	mothur_otu_file = args.mothur_otu_file
-	if mothur_otu_file is None:
-		print "Error -cl: Please pass a file containing a list of clusters in mothur format"
-		#parser.print_help()
-		sys.exit(1)
-
-	pool_size = args.pool_size
-	if pool_size < 1:
-		print "Error -p: only positive number of processors legal"
-		sys.exit(1)
-
-	if not os.path.isabs(metadata_table_in):
-		metadata_table_in = os.getcwd() + "/" + metadata_table_in
-
-	if not os.path.isabs(metadata_table_out):
-		#output_file = os.path.realpath(__file__)+"/"+output_file
-		metadata_table_out = os.getcwd() + "/" + metadata_table_out
-
-	if not os.path.isabs(input_querry_file):
-		input_querry_file = os.getcwd() + "/" + input_querry_file
-
-	if not os.path.isabs(input_ref_file):
-		input_ref_file = os.getcwd() + "/" + input_ref_file
-
-	if not os.path.isabs(mothur_otu_file):
-		mothur_otu_file = os.getcwd() + "/" + mothur_otu_file
-
-	folder_tools = os.path.dirname(os.path.realpath(__file__)) + "/tools"
-	file_config = folder_tools + "/config.cfg"
-	#print file_config
-	#config = None
-	with open(file_config, 'r') as config_handler:
-		config = Config(config_handler, 'PhyloPythiaS_Plus')
-	folder_database = config.get('databaseFile')
-	if not os.path.isabs(folder_database):
-		folder_database = os.path.normpath(folder_tools + "/" + folder_database)
-
-	if folder_database is None:
-		print("The taxonomy (databaseFile) is not specified.")
-		sys.exit(1)
-
-	taxonomy_db = None
-	if os.path.isdir(folder_database):
-		taxonomy_db = os.path.join(os.path.normpath(folder_database), 'ncbitax_sqlite.db')
-		if not os.path.isfile(taxonomy_db):
-			print("The directory '%s' doesn't contain sqllite taxonomy database file 'ncbitax_sqlite.db'")
-			sys.exit(1)
-
-	if not os.path.isfile(taxonomy_db):
-		print("The sqllite taxonomy database file doesn't exist:", taxonomy_db)
-		sys.exit(1)
-
-	ranks = args.ranks.strip().split(',')
-
-	unpublished_genomes_id_column_name = "genome_ID"
-	otu_cutoff_column_name = "cutoff"
-	ani_prediction_novelty_column_name = "ANI_NOVELTY_PREDICTION"
-	ani_prediction_column_name = "ANI_NCBI_TAXONOMIC_PREDICTION"
-	ani_scientific_name_column_name = "ANI_SCIENTIFIC_NAME"
-	ani_column_name = "ANI"
-
-	print "reading metadata table file:", metadata_table_in
-	metadata_table = MetaTable(metadata_table_in)
-	print "loading sqlite database:", taxonomy_db
-	taxonomy = TaxonomyNcbi(taxonomy_db)
-	print "reading mothur otu file:", mothur_otu_file
-	mothur_otus = MothurOTU(mothur_otu_file, taxonomy)
-	#print sorted(mothur_otus.otu_lists_by_cutoff_raw)
-	#print mothur_otus.to_string("0.002")
-	#sys.exit()
-
-	ani_scientific_name_column = metadata_table.get_column(ani_scientific_name_column_name)
-	if ani_scientific_name_column is None:
-		ani_scientific_name_column = [''] * metadata_table.number_of_rows
-
-	otu_cutoff_column = metadata_table.get_column(otu_cutoff_column_name)
-	if otu_cutoff_column is None:
-		otu_cutoff_column = [''] * metadata_table.number_of_rows
-
-	ani_prediction_novelty_column = metadata_table.get_column(ani_prediction_novelty_column_name)
-	if ani_prediction_novelty_column is None:
-		ani_prediction_novelty_column = [''] * metadata_table.number_of_rows
-
-	ani_prediction_column = metadata_table.get_column(ani_prediction_column_name)
-	if ani_prediction_column is None:
-		ani_prediction_column = [''] * metadata_table.number_of_rows
-
-	ani_column = metadata_table.get_column(ani_column_name)
-	if ani_column is None:
-		ani_column = [''] * metadata_table.number_of_rows
-
-	unpublished_genome_ids_column = metadata_table.get_column(unpublished_genomes_id_column_name)
-
-	logger = get_logger(True)
+	# TODO: get rid of import package
 	nucmer_exe = "importpackage mummer;nucmer"
-	with ANIm(input_querry_file, input_ref_file, None, nucmer_exe=nucmer_exe, logger=logger, pool_size=pool_size) as ani_calculator:
-		otus = mothur_otus.get_clusters(otu_cutoff_column, unpublished_genome_ids_column)
+	with ANIm(options.input_genomes_file, options.input_reference_file, None, nucmer_exe=nucmer_exe, logger=logger, pool_size=options.processors) as ani_calculator:
+		list_of_clusters = mothur_cluster.get_clusters_of_elements(cutoff_column, unpublished_genome_ids_column)
 		#logger.info("OTUS: {}".format(otus))
 		#sys.exit(0)
 		for unknown_genomes_id in unpublished_genome_ids_column:
-			if otus[unknown_genomes_id] is None:
+			if list_of_clusters[unknown_genomes_id] is None:
 				continue
-			ani_calculator.add_nucmer_cmd_lines(otus[unknown_genomes_id], [unknown_genomes_id])
+			ani_calculator.add_nucmer_cmd_lines(list_of_clusters[unknown_genomes_id], [unknown_genomes_id])
 
 		min_lengths, min_sim_errors, min_perc_ids, min_perc_aln, ncbi, ani_ish = ani_calculator.calculate_minimum_anim()
 
@@ -215,19 +62,14 @@ def my_main():
 					ani_prediction_novelty_column[row_index] = "same_species"
 				ani_prediction_column[row_index] = ncbi[unknown_genomes_id]
 				ani_column[row_index] = str(ani_ish[unknown_genomes_id])
-				science_name = taxonomy.getScientificName(ncbi[unknown_genomes_id])
+				science_name = taxonomy.get_scientific_name(ncbi[unknown_genomes_id])
 				if science_name is not None:
 					ani_scientific_name_column[row_index] = science_name
 
-	taxonomy.close()
-	metadata_table.set_column(ani_column_name, ani_column)
-	metadata_table.set_column(ani_prediction_novelty_column_name, ani_prediction_novelty_column)
-	metadata_table.set_column(ani_prediction_column_name, ani_prediction_column)
-	metadata_table.set_column(ani_scientific_name_column_name, ani_scientific_name_column)
-	metadata_table.write_meta_table_by_column(metadata_table_out)
-	print "ani prediction finished"
+	metadata_table.set_column(column_name_ani, ani_column)
+	metadata_table.set_column(column_name_ani_novelty, ani_prediction_novelty_column)
+	metadata_table.set_column(column_name_ani_taxid, ani_prediction_column)
+	metadata_table.set_column(column_name_ani_scientific_name, ani_scientific_name_column)
+	metadata_table.write(options.metadata_table_out)
+	logger.info("ANI prediction finished")
 	sys.exit(0)
-
-
-if __name__ == "__main__":
-	my_main()
--- cluster_prediction_to_meta_table.py
+++ cluster_prediction_to_meta_table.py
@@ -1,212 +1,119 @@
-#!/usr/bin/env python
-
 __author__ = 'hofmann'
 
-from source.MothurOTU import MothurOTU
+#from source.ArgumentHandler import ArgumentHandler
+from source.MothurCluster import MothurCluster
+from source.TaxonomicCluster import TaxonomicCluster
+from source.NcbiTaxonomy.NcbiTaxonomy import NcbiTaxonomy
 from source.MetaTable import MetaTable
-from source.TaxonomyNcbi import TaxonomyNcbi
-from source.config import Config
+from source.Logger import Logger
 import sys
 import os
-import argparse
 
 
-def my_main():
-	"""Parsing of arguments"""
-	# example:
-	epilog = '''
-	./tools/otu_prediction_to_meta_table.py
-	-i metadata_table_v7.csv
-	-o metadata_table_v7.out.csv
-	-cl marker_genes/16S_rRNA.fna.otu.txt..otu.txt
-	-db reference_db/nobackup/2014_02_01_ncbitax_sqlite.db
-'''
-
-	description = "Using clustering to make a taxonomic prediction that are added to a metatable"
-	parser = argparse.ArgumentParser(description=description)
-	#parser = argparse.ArgumentParser(description=description, epilog=epilog)
-
-	#parser.add_argument("-i", "--input_file", default=None, type=str,
-	#					help="path to file containing marker genes in multi fasta format")
-	parser.add_argument("-i", "--input_file", default=None, type=str,
-						help="path to file containing tab separated list of genomes and their file path")
-	parser.add_argument("-o", "--output_file", default=None, type=str,
-						help="result folder that will contain OTUs in mothur format")
-	parser.add_argument("-cl", "--cluster_file", default=None, type=str,
-						help="path to file containing tab separated list of genomes and their file path")
-	#parser.add_argument("-db", "--taxonomy_db", default=None, type=str,
-	#					help="path to a taxonomy sqlite database file")
-	parser.add_argument("-th", "--otu_threshold", default=0.03, type=float,
-						help="threshold used to determine otus. Default: 0.03")
-	parser.add_argument("-minth", "--min_threshold", default=0.02, type=float,
-						help="minimum threshold for classification. Default: 0.02")
-	options = parser.parse_args()
-
-	if options.input_file is None:
-		print "Error -i: Please pass a file containing a list of genome paths: <id>\t<path>"
-		#parser.print_help()
-		sys.exit(1)
-	else:
-		input_file = options.input_file
-		if not os.path.isabs(input_file):
-			input_file = os.getcwd() + "/" + input_file
-
-	if options.output_file is None:
-		print "Error -o: Please pass a file location for the results"
-		#parser.print_help()
-		sys.exit(1)
-	else:
-		output_file = options.output_file
-		if not os.path.isabs(output_file):
-			#output_file = os.path.realpath(__file__)+"/"+output_file
-			output_file = os.getcwd() + "/" + output_file
-
-	cluster_file = options.cluster_file
-	if cluster_file is None or not os.path.isfile(cluster_file):
-		print "Error -cl: Please pass a file containing a list of clusters in mothur format"
-		#parser.print_help()
-		sys.exit(1)
-	if not os.path.isfile(cluster_file):
-		print "Error -cl: File does not exist:", cluster_file
-		#parser.print_help()
-		sys.exit(1)
-
-	if not os.path.isabs(cluster_file):
-		cluster_file = os.getcwd() + "/" + cluster_file
-
-	folder_tools = os.path.dirname(os.path.realpath(__file__)) + "/tools"
-	file_config = folder_tools + "/config.cfg"
-	#print file_config
-	#config = None
-	with open(file_config, 'r') as config_handler:
-		config = Config(config_handler, 'PhyloPythiaS_Plus')
-	folder_database = config.get('databaseFile')
-	if not os.path.isabs(folder_database):
-		folder_database = os.path.normpath(folder_tools + "/" + folder_database)
-
-	if folder_database is None:
-		print("The taxonomy (databaseFile) is not specified.")
-		sys.exit(1)
-
-	taxonomy_db = None
-	if os.path.isdir(folder_database):
-		taxonomy_db = os.path.join(os.path.normpath(folder_database), 'ncbitax_sqlite.db')
-		if not os.path.isfile(taxonomy_db):
-			print("The directory '%s' doesn't contain sqllite taxonomy database file 'ncbitax_sqlite.db'")
-			sys.exit(1)
-
-	if not os.path.isfile(taxonomy_db):
-		print("The sqllite taxonomy database file doesn't exist:", taxonomy_db)
-		sys.exit(1)
-
-	#print input_file
-	#if args.step == 0 or args.step == 1:
-	#	detect_marker_genes(input_file, output_folder, processors)
-	#if args.step == 0 or args.step == 2:
-	#	cluster_marker_genes(input_file, output_folder, alignments_reference_db, threshold, distance_method, processors)
-
-	unpublished_genomes_id_column_name = "genome_ID"
-	otu_id_column_name = "OTU_16S"
-	otu_prediction_column_name = "OTU_NCBI_TAXONOMIC_PREDICTION"
-	otu_prediction_sname_column_name = "OTU_SCIENTIFIC_NAME"
-	otu_prediction_novelty_column_name = "OTU_NOVELTY_PREDICTION"
-	otu_cutoff_column_name = "cutoff"
-
-	print "reading metadata table file:", input_file
-	metadata_table = MetaTable(input_file)
+def my_main(options):
+	logger = Logger()
+	logger.info("Reading metadata table file: '{}'".format(options.metadata_table_in))
+	metadata_table = MetaTable('\t', logger)
+	metadata_table.read(options.metadata_table_in)
 	metadata_table.remove_empty_columns()
-	print "loading sqlite database:", taxonomy_db
-	taxonomy = TaxonomyNcbi(taxonomy_db)
-	print "reading mothur otu file:", cluster_file
-	mothur_otus = MothurOTU(cluster_file, taxonomy)
-	#print sorted(mothur_otus.otu_lists_by_cutoff_raw)
-	#print mothur_otus.to_string("0.002")
-	#sys.exit()
 
-	#otu_cutoff_column = metadata_table.get_column(otu_cutoff_column_name)
-	#if otu_cutoff_column is None or True:
-	otu_cutoff_column = [''] * metadata_table.number_of_rows
+	logger.info("Loading taxonomic database: '{}'".format(options.ncbi_reference_directory))
+	taxonomy = NcbiTaxonomy(options.ncbi_reference_directory, False, logger)
 
-	otu_id_column = metadata_table.get_column(otu_id_column_name)
-	if otu_id_column is None:
-		otu_id_column = [''] * metadata_table.number_of_rows
+	cluster_file = os.path.join(options.project_directory, options.file_cluster_mg_16s)
+	logger.info("Reading mothur cluster file: '{}'".format(cluster_file))
+	mothur_cluster = MothurCluster()
+	with open(cluster_file, 'r') as file_handle:
+		mothur_cluster.read_mothur_clustering_file(file_handle)
 
-	#otu_ncbi_column = metadata_table.get_column(otu_prediction_column_name)
-	#if otu_ncbi_column is None:
-	otu_ncbi_column = [''] * metadata_table.number_of_rows
+	taxonomy_cluster = TaxonomicCluster(mothur_cluster, taxonomy, logger)
 
-	#otu_name_column = metadata_table.get_column(otu_prediction_sname_column_name)
-	#if otu_name_column is None:
-	otu_name_column = [''] * metadata_table.number_of_rows
+	column_name_unpublished_genomes_id = metadata_table.get_column(options.column_name_unpublished_genomes_id)
+	if column_name_unpublished_genomes_id is None:
+		logger.error("Meta data file does not contain the required header '{}'".format(column_name_unpublished_genomes_id))
+		sys.exit(1)
 
-	#otu_novelty_column = metadata_table.get_column(otu_prediction_novelty_column_name)
-	#if otu_novelty_column is None:
-	otu_novelty_column = [''] * metadata_table.number_of_rows
+	taxonomic_prediction(options, metadata_table, mothur_cluster, taxonomy_cluster, taxonomy, logger)
+	set_otu_id(options, metadata_table, mothur_cluster, logger)
+	metadata_table.write(options.metadata_table_out)
 
-	unpublished_genomes_id_column = metadata_table.get_column(unpublished_genomes_id_column_name)
-	number_of_unpublished = len(unpublished_genomes_id_column)
-	min_cutoff = str(options.min_threshold)
-	otu_threshold = options.otu_threshold
+
+def taxonomic_prediction(options, metadata_table, mothur_cluster, taxonomy_cluster, taxonomy, logger):
+	column_cutoff = metadata_table.get_empty_column()
+	column_ncbi_prediction = metadata_table.get_empty_column()
+	column_science_name = metadata_table.get_empty_column()
+	column_novelty = metadata_table.get_empty_column()
+	column_name_unpublished_genomes_id = metadata_table.get_column(options.column_name_unpublished_genomes_id)
+	if column_name_unpublished_genomes_id is None:
+		logger.error("Meta data file does not contain the required header '{}'".format(column_name_unpublished_genomes_id))
+		sys.exit(1)
+	number_of_genomes = len(column_name_unpublished_genomes_id)
+	classification_distance = str(options.classification_distance_minimum)
 	all_done = False
-	for cluster_cutoff in sorted(mothur_otus.lists_of_cluster_by_cutoff_raw):
+	sorted_lists_of_cutoffs = mothur_cluster.get_sorted_lists_of_cutoffs()
+	for cluster_cutoff in sorted_lists_of_cutoffs:
 		if all_done:
 			break
 		if cluster_cutoff == "unique":
 			continue
-		if float(cluster_cutoff) < float(min_cutoff):
+		if float(cluster_cutoff) < float(classification_distance):
 			continue
-		print "\n\n#cutoff", cluster_cutoff
-		#print "\nget {0} ncbis of ids in otus using cutoff {1}".format(rank, otu_cutoff)
-		mothur_otus.set_cutoff_raw(cluster_cutoff)
+		logger.info("#cutoff {}".format(cluster_cutoff))
 		all_done = True
-		# TODO: separate otu from classification
-		for row_index in range(0, number_of_unpublished):
-			unpublished_genome_ids = unpublished_genomes_id_column[row_index]
-			#metadata_table.get_entry_index(unknown_genomes_id_column_name, unknown_genomes_id)
-			if unpublished_genome_ids != "" and otu_cutoff_column[row_index] == "" and mothur_otus.genome_id_exists(unpublished_genome_ids):
-				all_done = False
-				separator = ""
-				#print "{0} of {1}: {2}".format(row_index + 1, number_of_unknown, unknown_genomes_id)
-				list_of_cluster_id, list_of_cluster = mothur_otus.get_cluster_of(unpublished_genome_ids)
-				if len(list_of_cluster_id) > 1:
-					separator = ";"
-				otu_ncbi = []
-				otu_name = []
-				otu_novelty = []
-				otu_id = []
-				for index in range(0, len(list_of_cluster_id)):
-					cluster_id = list_of_cluster_id[index]
-					cluster = list_of_cluster[index]
-					if cluster is not None:
-						ncbi_prediction, novelty = mothur_otus.get_cluster_ncbi_tax_prediction(cluster, unpublished_genomes_id_column, unpublished_genome_ids)
-						if ncbi_prediction is not None and ncbi_prediction not in otu_ncbi:
-							otu_cutoff_column[row_index] = str(cluster_cutoff)
-							otu_ncbi.append(ncbi_prediction)
-							otu_name.append(taxonomy.getScientificName(ncbi_prediction))
-							otu_novelty.append("new_" + novelty)
-							otu_id.append(str(ncbi_prediction) + "." + str(cluster_id))
-				otu_ncbi_column[row_index] = separator.join(otu_ncbi)
-				otu_name_column[row_index] = separator.join(otu_name)
-				otu_novelty_column[row_index] =separator.join(otu_novelty)
-				if float(cluster_cutoff) == otu_threshold:
-					otu_id_column[row_index] = separator.join(otu_id)
-						#else:
-						#	print otu, "unknown species ncbi"
-					#else:
-						#otu_ncbi_column[row_index] = 1
-						#print "no otu found for:", unknown_genomes_id
+		for row_index in range(0, number_of_genomes):
+			unpublished_genome_ids = column_name_unpublished_genomes_id[row_index]
+			if not mothur_cluster.element_exists(cluster_cutoff, unpublished_genome_ids):
+				#logger.warning("{}: No cluster found for id '{}'".format(cluster_cutoff, unpublished_genome_ids))
+				continue
+			if unpublished_genome_ids == "" or column_ncbi_prediction[row_index] != "":
+				continue
+			all_done = False
+			separator = ""
+			list_of_cluster_id, list_of_cluster = mothur_cluster.get_cluster_of_cutoff_of_element(cluster_cutoff, unpublished_genome_ids)
+			if len(list_of_cluster_id) > 1:
+				separator = ";"
+			otu_ncbi = []
+			otu_name = []
+			otu_novelty = []
+			for index in range(0, len(list_of_cluster_id)):
+				cluster = list_of_cluster[index]
+				if cluster is None:
+					continue
+				ncbi_prediction, novelty = taxonomy_cluster.get_cluster_ncbi_tax_prediction(cluster, column_name_unpublished_genomes_id, unpublished_genome_ids)
+				if ncbi_prediction is not None and ncbi_prediction not in otu_ncbi:
+					column_cutoff[row_index] = str(cluster_cutoff)
+					otu_ncbi.append(ncbi_prediction)
+					otu_name.append(taxonomy.get_scientific_name(ncbi_prediction))
+					otu_novelty.append("new_" + novelty)
+			column_ncbi_prediction[row_index] = separator.join(otu_ncbi)
+			column_science_name[row_index] = separator.join(otu_name)
+			column_novelty[row_index] = separator.join(otu_novelty)
 
-	metadata_table.set_column(otu_id_column_name, otu_id_column)
-	metadata_table.set_column(otu_cutoff_column_name, otu_cutoff_column)
-	metadata_table.set_column(otu_prediction_column_name, otu_ncbi_column)
-	metadata_table.set_column(otu_prediction_sname_column_name, otu_name_column)
-	metadata_table.set_column(otu_prediction_novelty_column_name, otu_novelty_column)
-	metadata_table.write_meta_table_by_column(output_file)
-	taxonomy.close()
-	print "otu prediction finished"
-	sys.exit(0)
+	metadata_table.set_column(column_cutoff, options.column_name_cutoff)
+	metadata_table.set_column(column_ncbi_prediction, options.column_name_cluster_prediction)
+	metadata_table.set_column(column_science_name, options.column_name_cluster_scientific_name)
+	metadata_table.set_column(column_novelty, options.column_name_cluster_novelty)
+	logger.info("Taxonomic prediction finished")
 
 
-if __name__ == "__main__":
-	#help(start_process)
-	my_main()
+def set_otu_id(options, metadata_table, mothur_cluster, logger):
+	column_name_unpublished_genomes_id = metadata_table.get_column(options.column_name_unpublished_genomes_id)
+	number_of_genomes = len(column_name_unpublished_genomes_id)
+	otu_distance = options.otu_distance
+	column_otu_id = metadata_table.get_empty_column()
+	sorted_lists_of_cutoffs = mothur_cluster.get_sorted_lists_of_cutoffs()
+	for cluster_cutoff in sorted_lists_of_cutoffs:
+		if cluster_cutoff == "unique" or float(cluster_cutoff) != float(otu_distance):
+			continue
+		for row_index in range(0, number_of_genomes):
+			unpublished_genome_ids = column_name_unpublished_genomes_id[row_index]
+			if not mothur_cluster.element_exists(cluster_cutoff, unpublished_genome_ids):
+				logger.warning("{}: No cluster found for id '{}'".format(cluster_cutoff, unpublished_genome_ids))
+				continue
+			separator = ""
+			list_of_cluster_id, list_of_cluster = mothur_cluster.get_cluster_of_cutoff_of_element(cluster_cutoff, unpublished_genome_ids)
+			if len(list_of_cluster_id) > 1:
+				separator = ";"
+			column_otu_id[row_index] = separator.join([str(otu_id) for otu_id in list_of_cluster_id])
+	metadata_table.set_column(column_otu_id, options.column_name_otu_id)
+	logger.info("OTU finished")
