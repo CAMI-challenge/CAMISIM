2072500786bf1f7dcb74b622a42aabe366882c0b
p-hofmann <peter.hofmann@hhu.de>
2014-12-01 12:20:51 +0000

    preserve lowest novelty prediction

--- cluster_prediction_to_meta_table.py
+++ cluster_prediction_to_meta_table.py
@@ -45,6 +45,7 @@ def taxonomic_prediction(options, metadata_table, mothur_cluster, taxonomy_clust
 		sys.exit(1)
 	#_____statistic = {}
 	number_of_genomes = len(column_name_unpublished_genomes_id)
+	lowest_predicted_novelty = [''] * number_of_genomes
 	classification_distance = str(options.classification_distance_minimum)
 	all_done = False
 	sorted_lists_of_cutoffs = mothur_cluster.get_sorted_lists_of_cutoffs()
@@ -53,8 +54,6 @@ def taxonomic_prediction(options, metadata_table, mothur_cluster, taxonomy_clust
 			break
 		if cluster_cutoff == "unique":
 			continue
-		if float(cluster_cutoff) < float(classification_distance):
-			continue
 		logger.info("#threshold {}".format(cluster_cutoff))
 
 		#if cluster_cutoff not in _____statistic:
@@ -62,29 +61,32 @@ def taxonomic_prediction(options, metadata_table, mothur_cluster, taxonomy_clust
 
 		all_done = True
 		for row_index in range(0, number_of_genomes):
-			unpublished_genome_ids = column_name_unpublished_genomes_id[row_index]
-			if not mothur_cluster.element_exists(cluster_cutoff, unpublished_genome_ids):
+			unpublished_genome_id = column_name_unpublished_genomes_id[row_index]
+			if not mothur_cluster.element_exists(cluster_cutoff, unpublished_genome_id):
 				# if no marker gene was found it will not be in the clustering
 				continue
-			if unpublished_genome_ids == "" or column_ncbi_prediction[row_index] != "":
+			if unpublished_genome_id == "" or column_ncbi_prediction[row_index] != "":
 				continue
 			all_done = False
 			separator = ""
-			list_of_cluster_id, list_of_cluster = mothur_cluster.get_cluster_of_cutoff_of_element(cluster_cutoff, unpublished_genome_ids)
+			list_of_cluster_id, list_of_cluster = mothur_cluster.get_cluster_of_cutoff_of_element(cluster_cutoff, unpublished_genome_id)
 			if len(list_of_cluster) > 1:
 				separator = ";"
 			predicted__ncbi = []
 			predicted_science_name = []
-			predicted_novelty = []
+			#predicted_novelty = []
 			#list_support = []
 			for cluster in list_of_cluster:
 				#ncbi_prediction, novelty = taxonomy_cluster.get_cluster_ncbi_tax_prediction(cluster, column_name_unpublished_genomes_id, unpublished_genome_ids)
-				ncbi_prediction, novelty, support = taxonomy_cluster.predict_tax_id_of(cluster, column_name_unpublished_genomes_id, unpublished_genome_ids)
-				if ncbi_prediction is not None and ncbi_prediction not in predicted__ncbi:
+				ncbi_prediction, novelty, support = taxonomy_cluster.predict_tax_id_of(cluster, column_name_unpublished_genomes_id, unpublished_genome_id, lowest_predicted_novelty[row_index])
+				if ncbi_prediction is not None:
+					lowest_predicted_novelty[row_index] = novelty
+					if float(cluster_cutoff) < float(classification_distance) or ncbi_prediction in predicted__ncbi:
+						continue
 					column_cutoff[row_index] = str(cluster_cutoff)
 					predicted__ncbi.append(ncbi_prediction)
 					predicted_science_name.append(taxonomy.get_scientific_name(ncbi_prediction))
-					predicted_novelty.append("new_" + novelty)
+					#predicted_novelty.append("new_" + novelty)
 
 			#		list_support.append(support)
 			#_____statistic[cluster_cutoff]["sname"][row_index] = separator.join(predicted_science_name)
@@ -93,7 +95,8 @@ def taxonomic_prediction(options, metadata_table, mothur_cluster, taxonomy_clust
 
 			column_ncbi_prediction[row_index] = separator.join(predicted__ncbi)
 			column_science_name[row_index] = separator.join(predicted_science_name)
-			column_novelty[row_index] = separator.join(predicted_novelty)
+			if lowest_predicted_novelty[row_index] is not '':
+				column_novelty[row_index] = separator.join("new_" + lowest_predicted_novelty[row_index])
 
 	metadata_table.set_column(column_cutoff, options.column_name_cutoff)
 	metadata_table.set_column(column_ncbi_prediction, options.column_name_cluster_prediction)
@@ -127,13 +130,13 @@ def set_otu_id(options, metadata_table, mothur_cluster, logger):
 		if cluster_cutoff == "unique" or float(cluster_cutoff) != float(otu_distance):
 			continue
 		for row_index in range(0, number_of_genomes):
-			unpublished_genome_ids = column_name_unpublished_genomes_id[row_index]
-			if not mothur_cluster.element_exists(cluster_cutoff, unpublished_genome_ids):
+			unpublished_genome_id = column_name_unpublished_genomes_id[row_index]
+			if not mothur_cluster.element_exists(cluster_cutoff, unpublished_genome_id):
 				# if no marker gene was found it will not be in the clustering
-				list_of_unclustered_elements.add(unpublished_genome_ids)
+				list_of_unclustered_elements.add(unpublished_genome_id)
 				continue
 			separator = ""
-			list_of_cluster_id, list_of_cluster = mothur_cluster.get_cluster_of_cutoff_of_element(cluster_cutoff, unpublished_genome_ids)
+			list_of_cluster_id, list_of_cluster = mothur_cluster.get_cluster_of_cutoff_of_element(cluster_cutoff, unpublished_genome_id)
 			if len(list_of_cluster_id) > 1:
 				separator = ";"
 			column_otu_id[row_index] = separator.join([str(otu_id) for otu_id in sorted(set(list_of_cluster_id))])
