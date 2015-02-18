e49780317f9380d304ced4b8c442ba80dfbf84dd
p-hofmann <peter.hofmann@hhu.de>
2015-02-18 01:42:34 +0100

    return if file contains no valid sequences

--- tools/16SDetector/run.py
+++ tools/16SDetector/run.py
@@ -182,6 +182,10 @@ def main():
 
 	# generates working fasta files always when the configuration file is newer than particular files to be generated
 	sequences = Sequences(input_fasta_file, input_fasta_scaffolds_file, scaffolds_to_contigs_map_file, taxonomic_ranks, min_seq_len)
+	if sequences.get_sequence_count() == 0:
+		sys.stderr.write("WARNING: [16S detector] File contains no valid sequences: {}\n".format(input_fasta_file))
+		return
+
 	m_time_config = os.path.getmtime(config.getConfigFile())
 	if ((not os.path.isfile(fasta_file_ids) or not os.path.isfile(seq_name_seq_id_file) or not os.path.isfile(scaffold_contig_map_ids_file)) or
 			(m_time_config > min(os.path.getmtime(fasta_file_ids), os.path.getmtime(seq_name_seq_id_file), os.path.getmtime(scaffold_contig_map_ids_file)))):
