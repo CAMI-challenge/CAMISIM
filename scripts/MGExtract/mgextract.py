c75a003699de9740d18160e512197ca3b9f014ac
p-hofmann <peter.hofmann@hhu.de>
2015-10-05 15:49:16 +0200

    adding asserts and docstring

--- scripts/MGExtract/mgextract.py
+++ scripts/MGExtract/mgextract.py
@@ -14,106 +14,184 @@ class MGExtract(SequenceValidator):
 
 	_label = "MGExtract"
 
+	_suffixes = {
+		"5S": "5S_rRNA",
+		"16S": "16S_rRNA",
+		"23S": "23S_rRNA",
+		"8S": "8S_rRNA",
+		"18S": "18S_rRNA",
+		"28S": "28S_rRNA"}
+
+	_min_lengths = {
+		"5S": 100,
+		"16S": 900,
+		"23S": 1000,
+		"8S": 100,
+		"18S": 900,
+		"28S": 1000}
+
 	def __init__(
-		self, mg_analyse_executable, filename_query_genome_file_paths, filename_reference_genome_file_paths,
-		filename_reference_marker_genes, config_path, filename_iid_map=None, max_processors=1, temp_directory=None,
+		self, mg_analyse_executable, file_path_query_genome_file_paths, file_path_reference_genome_file_paths,
+		file_path_name_reference_marker_genes, config_path, file_path_iid_map=None, max_processors=1, temp_directory=None,
 		separator="\t", logfile=None, verbose=False, debug=False):
+		"""
+		Constructor
+
+		@param mg_analyse_executable: File path to modified tool of Ivan
+		@type mg_analyse_executable: str | unicode
+		@param file_path_query_genome_file_paths: File path to file with the location of genomes to be classified
+		@type file_path_query_genome_file_paths: str | unicode
+		@param file_path_reference_genome_file_paths: File path to file with the location of reference genomes
+		@type file_path_reference_genome_file_paths: str | unicode
+		@param file_path_name_reference_marker_genes: File path to fasta file with list of marker gene sequences
+		@type file_path_name_reference_marker_genes: str | unicode
+		@param config_path: File path to configuration file
+		@type config_path: str | unicode
+		@param file_path_iid_map:
+		@type file_path_iid_map:
+		@param max_processors: Amount of available processors
+		@type max_processors: int | long
+		@param temp_directory: File path to temporary storage
+		@type temp_directory: str | unicode
+		@param separator: Separator of metadata files
+		@type separator: str | unicode
+		"""
 		super(MGExtract, self).__init__(logfile=logfile, verbose=verbose, debug=debug)
-		assert filename_iid_map is None or (isinstance(filename_iid_map, basestring) and os.path.isfile(filename_iid_map))
-		assert os.path.isfile(filename_query_genome_file_paths)
-		assert os.path.isfile(filename_reference_genome_file_paths)
-		assert os.path.isfile(filename_reference_marker_genes)
-		assert os.path.isfile(config_path)
+		assert file_path_iid_map is None or self.validate_file(file_path_iid_map)
+		assert self.validate_file(file_path_query_genome_file_paths)
+		assert self.validate_file(file_path_reference_genome_file_paths)
+		assert self.validate_file(file_path_name_reference_marker_genes)
+		assert self.validate_file(config_path)
+		assert self.validate_file(mg_analyse_executable, executable=True)
+		assert self.validate_number(max_processors, minimum=1)
+		assert self.validate_dir(temp_directory)
 		self._temp_directory = temp_directory
 		self._mg_analyse_executable = mg_analyse_executable
-		self._filename_query_genome_file_paths = filename_query_genome_file_paths
-		self._filename_reference_genome_file_paths = filename_reference_genome_file_paths
-		self._filename_reference_marker_genes = filename_reference_marker_genes
+		self._file_path_query_genome_file_paths = file_path_query_genome_file_paths
+		self._file_path_reference_genome_file_paths = file_path_reference_genome_file_paths
+		self._file_path_reference_marker_genes = file_path_name_reference_marker_genes
 		self._config_path = config_path
 		self._max_processors = max_processors
 		self._debug = debug
 		self._working_dir = tempfile.mkdtemp(dir=self._temp_directory)
 		self._iid_map = None
-		if filename_iid_map is None:
+		self._separator = separator
+		if file_path_iid_map is None:
 			return
-		metadatatable = MetadataTable(
-			separator=separator,
+		meta_data_table = MetadataTable(
+			separator=self._separator,
 			logfile=logfile,
 			verbose=verbose)
-		metadatatable.read(filename_iid_map, column_names=False)
-		self._iid_map = metadatatable.get_map(0, 1)
-		del metadatatable
+		meta_data_table.read(file_path_iid_map, column_names=False)
+		self._iid_map = meta_data_table.get_map(0, 1)
+		del meta_data_table
 
 	def gather_markergenes(self, hmmer, mg_type, output_file):
-		self._logger.info("[MGExtract] Searching and extracting marker genes")
+		"""
+		Find and extract marker genes from genomes
+
+		@param hmmer: hmmer2 or hmmer3
+		@type hmmer: int | long
+		@param mg_type: '16S', '5S' or '23S' etc
+		@type mg_type: str | unicode
+		@param output_file: Output for list of extracted marker genes sequences in fasta format
+		@type output_file: str | unicode
+
+		@rtype: None
+		"""
+		assert isinstance(hmmer, (int, long))
+		assert isinstance(output_file, basestring)
+		assert self.validate_number(hmmer, minimum=2, maximum=3)
+		assert self.validate_dir(output_file, only_parent=True)
+		assert mg_type in self._suffixes, "Marker gene '{}' is not supported."
+
+		self._logger.info("Searching and extracting marker genes")
 		start = time.time()
-		success = True
-		query_genome_file_paths = self._parse_genome_file_path_file(self._filename_query_genome_file_paths)
-		if self._filename_reference_genome_file_paths is not None and self._filename_reference_marker_genes is None:
-			reference_genome_file_paths = self._parse_genome_file_path_file(self._filename_reference_genome_file_paths)
+		query_genome_file_paths = self._get_genome_id_to_path_map(self._file_path_query_genome_file_paths)
+		if self._file_path_reference_genome_file_paths is not None and self._file_path_reference_marker_genes is None:
+			reference_genome_file_paths = self._get_genome_id_to_path_map(self._file_path_reference_genome_file_paths)
 			query_genome_file_paths.update(reference_genome_file_paths)
-		elif self._filename_reference_genome_file_paths is not None and self._filename_reference_marker_genes is not None:
-			self._logger.warning("[MGExtract] Ignoring reference genome file paths and using previous reference marker genes!")
-		local_genome_file_paths = self._get_local_genome_paths(query_genome_file_paths)
+		elif self._file_path_reference_genome_file_paths is not None and self._file_path_reference_marker_genes is not None:
+			self._logger.warning("Ignoring reference genome file paths and using previous reference marker genes!")
 
 		# establish symbolic link to fasta files
+		local_genome_file_paths = self._get_local_genome_paths(query_genome_file_paths)
 		for genome_id, src in query_genome_file_paths.iteritems():
 			dst = local_genome_file_paths[genome_id]
-			if not os.path.exists(src):
-				self._logger.error("[MGExtract] File does not exist: '{}'".format(src))
+			if not self.validate_file(src):
 				if not self._debug:
 					shutil.rmtree(self._working_dir)
 				else:
-					self._logger.warning("[MGExtract] Remove manually: '{}'".format(self._working_dir))
+					self._logger.warning("Remove manually: '{}'".format(self._working_dir))
 				return False
 			os.symlink(src, dst)
 
-		cmd_task_list = self._create_cmd_task_list(hmmer=hmmer, list_of_fasta=local_genome_file_paths.values())
-		fail_list = parallel.runCmdParallel(cmd_task_list, self._max_processors)
+		list_of_tasks = self._get_cmd_task_list(hmmer=hmmer, list_of_fasta=local_genome_file_paths.values())
+		fail_list = parallel.runCmdParallel(list_of_tasks, self._max_processors)
 		if fail_list is not None:
-			parallel.reportFailedCmd(fail_list)
-			success = False
+			for message in parallel.reportFailedCmd(fail_list):
+				self._logger.error(message)
+			msg = "Extracting marker genes failed."
+			self._logger.error(msg)
+			raise OSError(msg)
 
-		if success:
-			tmp_out_file_path = tempfile.mktemp(suffix="_accepted", dir=self._working_dir)
-			tmp_out_file_bin_path = tempfile.mktemp(suffix="_rejected", dir=self._working_dir)
+		tmp_out_file_path = tempfile.mktemp(suffix="_accepted", dir=self._working_dir)
+		tmp_out_file_bin_path = tempfile.mktemp(suffix="_rejected", dir=self._working_dir)
 
-			self._merge_marker_genes_files(local_genome_file_paths, tmp_out_file_path, out_bin_file=tmp_out_file_bin_path, mg_type=mg_type)
-			if os.path.exists(tmp_out_file_path):
-				shutil.copy2(tmp_out_file_path, output_file)
-			else:
-				self._logger.warning("[MGExtract] No valid maker gene found!")
-				success = False
-			if os.path.exists(tmp_out_file_bin_path):
-				shutil.copy2(tmp_out_file_bin_path, output_file + ".rejected.fna")
-
-			if self._filename_reference_marker_genes is not None:
-				# append reference genome marker genes
-				shutil.copy(output_file, output_file + ".no_ref")
-				with open(output_file, 'a') as write_handler, open(self._filename_reference_marker_genes) as read_handler:
-					write_handler.writelines(read_handler)
-		end = time.time()
-		if success:
-			self._logger.info("[MGExtract] Done ({}s)".format(round(end - start, 1)))
-			self._logger.info("[MGExtract] Output: {}".format(output_file))
+		self._merge_marker_genes_files(local_genome_file_paths, tmp_out_file_path, out_bin_file_path=tmp_out_file_bin_path, mg_type=mg_type)
+		if os.path.exists(tmp_out_file_path):
+			shutil.copy2(tmp_out_file_path, output_file)
 		else:
-			self._logger.info("[MGExtract] Failed ({}s)".format(round(end - start, 1)))
+			self._logger.warning("No valid maker gene found!")
+		if os.path.exists(tmp_out_file_bin_path):
+			shutil.copy2(tmp_out_file_bin_path, output_file + ".rejected.fna")
+
+		if self._file_path_reference_marker_genes is not None:
+			# append reference genome marker genes
+			shutil.copy(output_file, output_file + ".no_ref")
+			with open(output_file, 'a') as write_handler, open(self._file_path_reference_marker_genes) as read_handler:
+				write_handler.writelines(read_handler)
+
+		end = time.time()
+		self._logger.info("Extracting marker genes finished ({}s)".format(round(end - start, 1)))
 
 		if not self._debug:
 			shutil.rmtree(self._working_dir)
 		else:
-			self._logger.warning("[MGExtract] Remove manually: '{}'".format(self._working_dir))
-		return success
+			self._logger.warning("Remove manually: '{}'".format(self._working_dir))
 
-	def _create_cmd_task_list(self, hmmer, list_of_fasta):
+	def _get_cmd_task_list(self, hmmer, list_of_fasta):
+		"""
+		Get list of command tasks
+
+		@param hmmer: hmmer2 or hmmer3
+		@type hmmer: int | long
+		@param list_of_fasta: List of file paths of genomes
+		@type list_of_fasta: list[str|unicode]
+
+		@return: List of command tasks
+		@rtype: list[TaskCmd]
+		"""
+		assert isinstance(hmmer, (int, long))
+		assert isinstance(list_of_fasta, list)
+		assert self.validate_number(hmmer, minimum=2, maximum=3)
 		out_dir = self._working_dir
-		# TODO: use logfile instead of /dev/null, idealy detect errors
-		# cmd = "{exe} -c '{config}' -nn -hmmer {hmmer} -i '{input_file}' -out '{out_dir}' > /dev/null 2> /dev/null"
 		cmd = "{exe} -c '{config}' -nn -hmmer {hmmer} -i '{input_file}' -out '{out_dir}'"
 		cmd_list = [cmd.format(exe=self._mg_analyse_executable, config=self._config_path, hmmer=hmmer, input_file=file_path, out_dir=out_dir) for file_path in list_of_fasta]
 		return [parallel.TaskCmd(cmd, out_dir) for cmd in cmd_list]
 
 	def _get_local_genome_paths(self, dict_genome_id_to_path):
+		"""
+		Create local paths for genomes to avoid several issues with mothur.
+
+		@param dict_genome_id_to_path: Map of genome id to the real path of genomes
+		@type dict_genome_id_to_path: dict[str|unicode, str|unicode]
+
+		@return: Map of genome id to the local path of genomes
+		@rtype: dict[str|unicode, str|unicode]
+		"""
+		assert isinstance(dict_genome_id_to_path, dict)
+
 		system_link_directory = os.path.join(self._working_dir, "sym_links")
 		if not os.path.exists(system_link_directory):
 			os.makedirs(system_link_directory)
@@ -123,41 +201,52 @@ class MGExtract(SequenceValidator):
 			dict_genome_id_to_local_path[genome_id] = os.path.join(system_link_directory, basename)
 		return dict_genome_id_to_local_path
 
-	@staticmethod
-	def _parse_genome_file_path_file(file_path):
-		dict_genome_id_to_path = {}
-		with open(file_path) as read_handler:
-			for line in read_handler:
-				if line.startswith('#'):
-					continue
-				line = line.strip()
-				if len(line) == 0:
-					continue
-				genome_id, genome_path = line.split('\t')
-				dict_genome_id_to_path[genome_id] = genome_path
+	def _get_genome_id_to_path_map(self, file_path):
+		"""
+		Get a map of genome_id to genome path
+
+		@param file_path: File path
+		@type file_path: str | unicode
+
+		@return: map of genome_id to genome path
+		@rtype: dict[str|unicode, str|unicode]
+		"""
+		assert self.validate_file(file_path)
+
+		data_table = MetadataTable(
+			separator=self._separator,
+			logfile=self._logfile,
+			verbose=self._verbose)
+		data_table.read(file_path, column_names=False)
+		if data_table.get_number_of_rows() == 0:
+			self._logger.warning("No data in file '{}'.".format(file_path))
+			return {}
+		dict_genome_id_to_path = data_table.get_map(0, 1)
 		return dict_genome_id_to_path
 
-	# gather all marker gene sequences into single files
-	def _merge_marker_genes_files(self, dict_genome_id_to_path, out_file_path, out_bin_file, mg_type):
-		suffixes = {
-			"5S": "5S_rRNA",
-			"16S": "16S_rRNA",
-			"23S": "23S_rRNA",
-			"8S": "8S_rRNA",
-			"18S": "18S_rRNA",
-			"28S": "28S_rRNA"}
+	def _merge_marker_genes_files(self, dict_genome_id_to_path, out_file_path, out_bin_file_path, mg_type):
+		"""
+		Gather all marker gene sequences into single files
 
-		min_lengths = {
-			"5S": 100,
-			"16S": 900,
-			"23S": 1000,
-			"8S": 100,
-			"18S": 900,
-			"28S": 1000}
-		suffix = suffixes[mg_type]
-		min_length = min_lengths[mg_type]
+		@param dict_genome_id_to_path: Map of genome id to the real path of genomes
+		@type dict_genome_id_to_path: dict[str|unicode, str|unicode]
+		@param out_file_path: Fasta formatted output file path
+		@type out_file_path: str|unicode
+		@param out_bin_file_path: Fasta formatted output file path of rejected sequences
+		@type out_bin_file_path: str|unicode
+		@param mg_type: '16S', '5S' or '23S' etc
+		@type mg_type: str | unicode
+
+		@rtype: None
+		"""
+		assert mg_type in self._suffixes, "Marker gene '{}' is not supported."
 		assert isinstance(dict_genome_id_to_path, dict)
-		with open(out_file_path, "a") as output_file_handle, open(out_bin_file, "a") as out_bin_file_handle:
+		assert isinstance(out_file_path, basestring)
+		assert isinstance(out_bin_file_path, basestring)
+		suffix = self._suffixes[mg_type]
+		min_length = self._min_lengths[mg_type]
+
+		with open(out_file_path, "a") as output_file_handle, open(out_bin_file_path, "a") as out_bin_file_handle:
 			for genome_id, genome_path in dict_genome_id_to_path.iteritems():
 				input_filename = os.path.basename(genome_path)
 				input_filepath = "{prefix}.ids.{suffix}.fna".format(prefix=input_filename, suffix=suffix)
