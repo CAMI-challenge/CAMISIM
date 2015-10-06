bb18d19f8a53cb6036b4ee1b766e20bac4a84aab
p-hofmann <peter.hofmann@hhu.de>
2015-10-06 17:46:07 +0200

    adding of generation of a internal id map file

--- scripts/MGExtract/mgextract.py
+++ scripts/MGExtract/mgextract.py
@@ -1,11 +1,11 @@
 __author__ = 'Peter Hofmann'
 
 import os
+import time
 import shutil
 import tempfile
-import time
-import scripts.MGExtract.concat_fasta_on_fasta as concat_fasta_on_fasta
 import scripts.parallel as parallel
+from scripts.MGExtract.sequencemerger import SequenceMerger
 from scripts.Validator.sequencevalidator import SequenceValidator
 from scripts.MetaDataTable.metadatatable import MetadataTable
 
@@ -35,7 +35,7 @@ class MGExtract(SequenceValidator):
 
 	def __init__(
 		self, mg_analyse_executable, file_path_query_genome_file_paths, file_path_reference_genome_file_paths,
-		file_path_name_reference_marker_genes, config_path, file_path_iid_map=None, max_processors=1, temp_directory=None,
+		file_path_name_reference_marker_genes, config_path, file_path_map_reference_genome_id_to_tax_id=None, max_processors=1, temp_directory=None,
 		separator="\t", logfile=None, verbose=False, debug=False):
 		"""
 		Constructor
@@ -50,8 +50,8 @@ class MGExtract(SequenceValidator):
 		@type file_path_name_reference_marker_genes: str | unicode
 		@param config_path: File path to configuration file
 		@type config_path: str | unicode
-		@param file_path_iid_map:
-		@type file_path_iid_map:
+		@param file_path_map_reference_genome_id_to_tax_id: Mapping of Reference genome_id to their taxonomic assignment
+		@type file_path_map_reference_genome_id_to_tax_id: str | unicode
 		@param max_processors: Amount of available processors
 		@type max_processors: int | long
 		@param temp_directory: File path to temporary storage
@@ -60,7 +60,7 @@ class MGExtract(SequenceValidator):
 		@type separator: str | unicode
 		"""
 		super(MGExtract, self).__init__(logfile=logfile, verbose=verbose, debug=debug)
-		assert file_path_iid_map is None or self.validate_file(file_path_iid_map)
+		assert file_path_map_reference_genome_id_to_tax_id is None or self.validate_file(file_path_map_reference_genome_id_to_tax_id)
 		assert self.validate_file(file_path_query_genome_file_paths)
 		assert self.validate_file(file_path_reference_genome_file_paths)
 		assert self.validate_file(file_path_name_reference_marker_genes)
@@ -77,19 +77,19 @@ class MGExtract(SequenceValidator):
 		self._max_processors = max_processors
 		self._debug = debug
 		self._working_dir = tempfile.mkdtemp(dir=self._temp_directory)
-		self._iid_map = None
+		self._genome_id_to_tax_id = None
 		self._separator = separator
-		if file_path_iid_map is None:
+		if file_path_map_reference_genome_id_to_tax_id is None:
 			return
 		meta_data_table = MetadataTable(
 			separator=self._separator,
 			logfile=logfile,
 			verbose=verbose)
-		meta_data_table.read(file_path_iid_map, column_names=False)
-		self._iid_map = meta_data_table.get_map(0, 1)
+		meta_data_table.read(file_path_map_reference_genome_id_to_tax_id, column_names=False)
+		self._genome_id_to_tax_id = meta_data_table.get_map(0, 1)
 		del meta_data_table
 
-	def gather_markergenes(self, hmmer, mg_type, output_file):
+	def gather_markergenes(self, hmmer, mg_type, file_path_output, file_path_map_uid_sid):
 		"""
 		Find and extract marker genes from genomes
 
@@ -97,15 +97,15 @@ class MGExtract(SequenceValidator):
 		@type hmmer: int | long
 		@param mg_type: '16S', '5S' or '23S' etc
 		@type mg_type: str | unicode
-		@param output_file: Output for list of extracted marker genes sequences in fasta format
-		@type output_file: str | unicode
+		@param file_path_output: Output for list of extracted marker genes sequences in fasta format
+		@type file_path_output: str | unicode
 
 		@rtype: None
 		"""
 		assert isinstance(hmmer, (int, long))
-		assert isinstance(output_file, basestring)
+		assert isinstance(file_path_output, basestring)
 		assert self.validate_number(hmmer, minimum=2, maximum=3)
-		assert self.validate_dir(output_file, only_parent=True)
+		assert self.validate_dir(file_path_output, only_parent=True)
 		assert mg_type in self._suffixes, "Marker gene '{}' is not supported."
 
 		self._logger.info("Searching and extracting marker genes")
@@ -141,18 +141,20 @@ class MGExtract(SequenceValidator):
 		tmp_out_file_path = tempfile.mktemp(suffix="_accepted", dir=self._working_dir)
 		tmp_out_file_bin_path = tempfile.mktemp(suffix="_rejected", dir=self._working_dir)
 
-		self._merge_marker_genes_files(local_genome_file_paths, tmp_out_file_path, out_bin_file_path=tmp_out_file_bin_path, mg_type=mg_type)
+		self._merge_marker_genes_files(
+			local_genome_file_paths, tmp_out_file_path,
+			file_path_out_bin=tmp_out_file_bin_path, file_path_map_uid_sid=file_path_map_uid_sid, mg_type=mg_type)
 		if os.path.exists(tmp_out_file_path):
-			shutil.copy2(tmp_out_file_path, output_file)
+			shutil.copy2(tmp_out_file_path, file_path_output)
 		else:
 			self._logger.warning("No valid maker gene found!")
 		if os.path.exists(tmp_out_file_bin_path):
-			shutil.copy2(tmp_out_file_bin_path, output_file + ".rejected.fna")
+			shutil.copy2(tmp_out_file_bin_path, file_path_output + ".rejected.fna")
 
 		if self._file_path_reference_marker_genes is not None:
 			# append reference genome marker genes
-			shutil.copy(output_file, output_file + ".no_ref")
-			with open(output_file, 'a') as write_handler, open(self._file_path_reference_marker_genes) as read_handler:
+			shutil.copy(file_path_output, file_path_output + ".no_ref")
+			with open(file_path_output, 'a') as write_handler, open(self._file_path_reference_marker_genes) as read_handler:
 				write_handler.writelines(read_handler)
 
 		end = time.time()
@@ -227,16 +229,16 @@ class MGExtract(SequenceValidator):
 		dict_genome_id_to_path = data_table.get_map(0, 1)
 		return dict_genome_id_to_path
 
-	def _merge_marker_genes_files(self, dict_genome_id_to_path, out_file_path, out_bin_file_path, mg_type):
+	def _merge_marker_genes_files(self, dict_genome_id_to_path, file_path_out, file_path_out_bin, file_path_map_uid_sid, mg_type):
 		"""
 		Gather all marker gene sequences into single files
 
 		@param dict_genome_id_to_path: Map of genome id to the real path of genomes
 		@type dict_genome_id_to_path: dict[str|unicode, str|unicode]
-		@param out_file_path: Fasta formatted output file path
-		@type out_file_path: str|unicode
-		@param out_bin_file_path: Fasta formatted output file path of rejected sequences
-		@type out_bin_file_path: str|unicode
+		@param file_path_out: Fasta formatted output file path
+		@type file_path_out: str|unicode
+		@param file_path_out_bin: Fasta formatted output file path of rejected sequences
+		@type file_path_out_bin: str|unicode
 		@param mg_type: '16S', '5S' or '23S' etc
 		@type mg_type: str | unicode
 
@@ -244,18 +246,31 @@ class MGExtract(SequenceValidator):
 		"""
 		assert mg_type in self._suffixes, "Marker gene '{}' is not supported."
 		assert isinstance(dict_genome_id_to_path, dict)
-		assert isinstance(out_file_path, basestring)
-		assert isinstance(out_bin_file_path, basestring)
+		assert isinstance(file_path_out, basestring)
+		assert isinstance(file_path_out_bin, basestring)
 		suffix = self._suffixes[mg_type]
 		min_length = self._min_lengths[mg_type]
 
-		with open(out_file_path, "a") as output_file_handle, open(out_bin_file_path, "a") as out_bin_file_handle:
+		with open(file_path_out, "a") as stream_output,\
+			open(file_path_out_bin, "a") as stream_output_bin,\
+			open(file_path_map_uid_sid, "w") as stream_map_uid_sid:
+			counter = 0
+			sequence_merger = SequenceMerger(
+				stream_output=stream_output,
+				stream_output_bin=stream_output_bin,
+				stream_map_uid_sid=stream_map_uid_sid,
+				verbose=True)
 			for genome_id, genome_path in dict_genome_id_to_path.iteritems():
 				input_filename = os.path.basename(genome_path)
 				input_filepath = "{prefix}.ids.{suffix}.fna".format(prefix=input_filename, suffix=suffix)
 				input_file = os.path.join(self._working_dir, "working", input_filepath)
-				unique_id = genome_id
-				if self._iid_map is not None:
-					unique_id = self._iid_map[genome_id]
-				concat_fasta_on_fasta.merge(input_file, output_file_handle, min_length, unique_id=unique_id, out_bin_file_handle=out_bin_file_handle)
+				# unique_id = genome_id
+				unique_id = "GR_{}".format(counter)
+				tax_id = ""
+				if self._genome_id_to_tax_id is not None and genome_id in self._genome_id_to_tax_id:
+					tax_id = self._genome_id_to_tax_id[genome_id]
+				sequence_merger.merge(input_file, min_length=min_length, prefix_unique_id=unique_id, tax_id=tax_id)
+				counter += 1
+				# concat_fasta_on_fasta.merge(
+				# input_file, stream_output, min_length, unique_id=unique_id, out_bin_file_handle=stream_output_bin)
 				# input_file_path, output_file_handle, min_length, unique_id=None, out_bin_file_handle=None, uid_sid_file_handle=None
