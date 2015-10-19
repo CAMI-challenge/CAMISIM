7ca4c57537a10af4a32b0c54a1e64d4737318ec0
p-hofmann <peter.hofmann@hhu.de>
2015-10-19 13:53:29 +0200

    docstring + asserts added

--- scripts/MGCluster/mgcluster.py
+++ scripts/MGCluster/mgcluster.py
@@ -10,6 +10,12 @@ from scripts.Validator.validator import Validator
 
 
 class MGCluster(Validator):
+	"""
+	Alignment and clustering of marker genes with references
+	"""
+
+	_cluster_method_choices = ['average', 'furthest', 'nearest']
+	_silva_ref_files = ["mothur_ref_distances", "mothur_ref_names", "mothur_alignment_ref.fasta", "map.tsv"]
 
 	_mothur_cmd_ref_dist_split = """unique.seqs(fasta={mg_fasta})
 align.seqs(candidate=current, template={ref_align}, align=gotoh, flip=t, processors={processors})
@@ -36,6 +42,28 @@ cluster(cutoff={cutoff}, method={method}, precision={precision}, name={filename}
 	def __init__(
 		self, mothur_executable, directory_silva_reference, max_processors=1, temp_directory=None,
 		logfile=None, verbose=False, debug=False):
+		"""
+		Constructor
+
+		@param mothur_executable: File path to mothur binary
+		@type mothur_executable: str | unicode
+		@param directory_silva_reference: Path to directory with SILVA reference database files
+		@type directory_silva_reference: str | unicode
+		@param max_processors: Maximum number of available processors
+		@type max_processors: int | long
+		@param temp_directory: Directory for temporary data
+		@type temp_directory: str | unicode
+		@param logfile: file handler or file path to a log file
+		@type logfile: file | FileIO | StringIO | basestring
+		@param verbose: Not verbose means that only warnings and errors will be past to stream
+		@type verbose: bool
+		@param debug: Display debug messages
+		@type debug: bool
+		"""
+		assert self.validate_file(mothur_executable, executable=True)
+		assert self.validate_dir(directory_silva_reference, file_names=self._silva_ref_files)
+		assert self.validate_number(max_processors, minimum=1)
+		assert self.validate_dir(temp_directory)
 		super(MGCluster, self).__init__(logfile=logfile, verbose=verbose, debug=False)
 		self._mothur_executable = mothur_executable
 		self._temp_directory = temp_directory
@@ -58,6 +86,28 @@ cluster(cutoff={cutoff}, method={method}, precision={precision}, name={filename}
 		self._local_distance = "ref.align.dist"
 
 	def cluster(self, marker_gene_fasta, output_cluster_file, distance_cutoff, precision=1000, method="average"):
+		"""
+		CLuster Markergenes
+
+		@param marker_gene_fasta: Fasta formatted file with marker genes
+		@type marker_gene_fasta: str | unicode
+		@param output_cluster_file: Output of mg clustering
+		@type output_cluster_file: str | unicode
+		@param distance_cutoff: Exclude irrelevant higher distances before clustering
+		@type distance_cutoff: int | long
+		@param precision: Cluster are made in steps: 10: 0.1, 100: 0.01, 1000: 0.001
+		@type precision: int | long
+		@param method: Cluster algorithm 'average', 'furthest', 'nearest'
+		@type method: str | unicode
+
+		@return: Nothing
+		@rtype: None
+		"""
+		assert self.validate_file(marker_gene_fasta)
+		assert self.validate_dir(output_cluster_file, only_parent=True)
+		assert self.validate_number(distance_cutoff, minimum=0, maximum=1)
+		assert self.validate_number(precision, minimum=0)
+		assert method in self._cluster_method_choices
 		self._logger.info("[MGCluster] Starting clustering process")
 		start = time.time()
 		local_marker_gene_fasta = self._get_symbolic_link_path(marker_gene_fasta)
@@ -109,6 +159,16 @@ cluster(cutoff={cutoff}, method={method}, precision={precision}, name={filename}
 		return result
 
 	def _get_symbolic_link_path(self, original_file_path):
+		"""
+		Get path to local symbolic link since mothur might act odd else.
+
+		@param original_file_path:
+		@type original_file_path: str | unicode
+
+		@return: Local path
+		@rtype: str | unicode
+		"""
+		assert isinstance(original_file_path, basestring)
 		basename = os.path.basename(original_file_path)
 		new_path = os.path.join(self._working_dir, basename)
 		os.symlink(original_file_path, new_path)
@@ -116,6 +176,25 @@ cluster(cutoff={cutoff}, method={method}, precision={precision}, name={filename}
 		return basename
 
 	def _get_mothur_cmd(self, marker_gene_fasta, cutoff, precision, method="average"):
+		"""
+		Get command line to run mothur
+
+		@param marker_gene_fasta: Fasta formatted file with marker genes
+		@type marker_gene_fasta: str | unicode
+		@param cutoff: Exclude irrelevant higher distances before clustering
+		@type cutoff: int | long
+		@param precision: Cluster are made in steps: 10: 0.1, 100: 0.01, 1000: 0.001
+		@type precision: int | long
+		@param method: Cluster algorithm 'average', 'furthest', 'nearest'
+		@type method: str | unicode
+
+		@return: Command line
+		@rtype: str | unicode
+		"""
+		assert self.validate_file(marker_gene_fasta)
+		assert self.validate_number(cutoff, minimum=0, maximum=1)
+		assert self.validate_number(precision, minimum=0)
+		assert method in self._cluster_method_choices
 		# basename = os.path.basename(marker_gene_fasta)
 		# filename, extension = os.path.splitext(basename)
 		filename, extension = os.path.splitext(marker_gene_fasta)
