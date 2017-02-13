45e6ef61641d5db9f3015a8befa8dcbc80df33bd
p-hofmann <peter.hofmann@hhu.de>
2017-02-13 10:48:21 +0100

    read config file options mostly silently

--- scripts/argumenthandler.py
+++ scripts/argumenthandler.py
@@ -264,32 +264,32 @@ class ConfigFileHandler(DefaultValues):
 
 		section = None  # "Main"
 		if self._phase is None:
-			self._phase = self._config.get_value("phase", is_digit=True)
+			self._phase = self._config.get_value("phase", is_digit=True, silent=True)
 
 		if self._seed is None:
-			self._seed = self._config.get_value("seed")
+			self._seed = self._config.get_value("seed", silent=True)
 
 		if self._max_processors is None:
-			self._max_processors = self._config.get_value("max_processors", is_digit=True)
+			self._max_processors = self._config.get_value("max_processors", is_digit=True, silent=True)
 
 		if self._dataset_id is None:
-			self._dataset_id = self._config.get_value("dataset_id")
+			self._dataset_id = self._config.get_value("dataset_id", silent=True)
 
 		if self._directory_output is None:
 			self._directory_output = self._config.get_value("output_directory", is_path=True)
 
 		if self._tmp_dir is None:
-			config_value = self._config.get_value("temp_directory", is_path=True)
+			config_value = self._config.get_value("temp_directory", is_path=True, silent=True)
 			if config_value is not None:
 				assert self._validator.validate_dir(config_value)
 				self._tmp_dir = config_value
 
-		self._phase_gsa = self._config.get_value("gsa", is_boolean=True)
-		self._phase_pooled_gsa = self._config.get_value("pooled_gsa", is_boolean=True)
+		self._phase_gsa = self._config.get_value("gsa", is_boolean=True, silent=True)
+		self._phase_pooled_gsa = self._config.get_value("pooled_gsa", is_boolean=True, silent=True)
 
-		self._compresslevel = self._config.get_value("compress", is_digit=True)
+		self._compresslevel = self._config.get_value("compress", is_digit=True, silent=True)
 
-		self._phase_anonymize = self._config.get_value("anonymous", is_boolean=True)
+		self._phase_anonymize = self._config.get_value("anonymous", is_boolean=True, silent=True)
 
 		# ##########
 		# [ReadSimulator]
@@ -297,15 +297,15 @@ class ConfigFileHandler(DefaultValues):
 
 		section = None  # "ReadSimulator"
 		if self._sample_size_in_base_pairs is None:
-			config_value = self._config.get_value("size", is_digit=True)
+			config_value = self._config.get_value("size", is_digit=True, silent=True)
 			if config_value is not None:
 				self._sample_size_in_base_pairs = long(config_value * self._base_pairs_multiplication_factor)
 
 		if self._read_simulator_type is None:
-			self._read_simulator_type = self._config.get_value("type")
+			self._read_simulator_type = self._config.get_value("type", silent=True)
 
 		if self._executable_samtools is None:
-			self._executable_samtools = self._config.get_value("samtools", is_path=True)
+			self._executable_samtools = self._config.get_value("samtools", is_path=True, silent=True)
 
 		if self._executable_art_illumina is None:
 			self._executable_art_illumina = self._config.get_value("art_illumina", silent=True, is_path=True)
@@ -318,10 +318,10 @@ class ConfigFileHandler(DefaultValues):
 
 		if self._fragment_size_standard_deviation_in_bp is None:
 			self._fragment_size_standard_deviation_in_bp = self._config.get_value(
-				"fragment_size_standard_deviation", is_digit=True)
+				"fragment_size_standard_deviation", is_digit=True, silent=True)
 
 		if self._fragments_size_mean_in_bp is None:
-			self._fragments_size_mean_in_bp = self._config.get_value("fragments_size_mean", is_digit=True)
+			self._fragments_size_mean_in_bp = self._config.get_value("fragments_size_mean", is_digit=True, silent=True)
 
 		# ##########
 		# [CommunityDesign]
@@ -338,10 +338,10 @@ class ConfigFileHandler(DefaultValues):
 
 		if self._strain_simulation_template is None:
 			self._strain_simulation_template = self._config.get_value(
-				"strain_simulation_template", silent=True, is_path=True)
+				"strain_simulation_template", silent=True, is_path=True, silent=True)
 
 		if self._number_of_samples is None:
-			self._number_of_samples = self._config.get_value("number_of_samples", is_digit=True)
+			self._number_of_samples = self._config.get_value("number_of_samples", is_digit=True, silent=True)
 
 		# if self._number_of_communities is None:
 		# 	self._number_of_communities = self._config.get_value('number_of_communities', is_digit=True)
@@ -364,7 +364,7 @@ class ConfigFileHandler(DefaultValues):
 			file_path_metadata_table = self._config.get_value('metadata', community_section, is_path=True)
 			file_path_genome_locations = self._config.get_value('id_to_genome_file', community_section, is_path=True)
 			file_path_gff_locations = self._config.get_value('id_to_gff_file', community_section, is_path=True, silent=True)
-			mode = self._config.get_value('mode', community_section)
+			mode = self._config.get_value('mode', community_section, silent=True)
 			if not isinstance(file_path_metadata_table, basestring):
 				is_valid = False
 			if not isinstance(file_path_genome_locations, basestring):
@@ -384,17 +384,17 @@ class ConfigFileHandler(DefaultValues):
 				identifier=community_section,
 				genomes_total=self._config.get_value('genomes_total', community_section, is_digit=True),
 				genomes_real=self._config.get_value('genomes_real', community_section, is_digit=True),
-				limit_per_otu=self._config.get_value('max_strains_per_otu', community_section, is_digit=True),
+				limit_per_otu=self._config.get_value('max_strains_per_otu', community_section, is_digit=True, silent=True),
 				file_path_metadata_table=file_path_metadata_table,
 				file_path_genome_locations=file_path_genome_locations,
 				file_path_gff_locations=file_path_gff_locations,
-				ratio=self._config.get_value('ratio', community_section, is_digit=True),
+				ratio=self._config.get_value('ratio', community_section, is_digit=True, silent=True),
 				mode=mode,
-				log_mu=self._config.get_value('log_mu', community_section, is_digit=True),
-				log_sigma=self._config.get_value('log_sigma', community_section, is_digit=True),
-				gauss_mu=self._config.get_value('gauss_mu', community_section, is_digit=True),
-				gauss_sigma=self._config.get_value('gauss_sigma', community_section, is_digit=True),
-				verbose=self._config.get_value('view', community_section, is_boolean=True)
+				log_mu=self._config.get_value('log_mu', community_section, is_digit=True, silent=True),
+				log_sigma=self._config.get_value('log_sigma', community_section, is_digit=True, silent=True),
+				gauss_mu=self._config.get_value('gauss_mu', community_section, is_digit=True, silent=True),
+				gauss_sigma=self._config.get_value('gauss_sigma', community_section, is_digit=True, silent=True),
+				verbose=self._config.get_value('view', community_section, is_boolean=True, silent=True)
 			)
 			self._list_of_communities.append(new_community)
 			self._number_of_communities = len(self._list_of_communities)
